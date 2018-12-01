874a439d869fc848c7aed357db7fecf6d9799723
cowooding <jwjppp@naver.com>
2018-12-01 16:56:54 +0900

    1.2.7버전 한자리수 날짜에 대해서 대응하고 월이 바뀌는 점검에 대해서 대응한다.

--- checkark/app/src/main/java/com/cowooding/checkark/MainActivity.java
+++ checkark/app/src/main/java/com/cowooding/checkark/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mDatabase = FirebaseDatabase.getInstance();
 
         layout_main = (ConstraintLayout)findViewById(R.id.layout_main);
@@ -54,25 +55,30 @@ public class MainActivity extends AppCompatActivity {
 
         DatabaseReference mReference = mDatabase.getReference("docs/titles"); // 변경값을 확인할 child 이름
         mReference.addValueEventListener(new ValueEventListener() {
+            //데이터 변화가 있을 때 시간을 측정
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String time = getTime();
+                //윤년 계산을 위해
+                int year = Integer.valueOf(time.substring(0,4));
+                //한자리수에도 대응하기 위해 int
+                int month = setMonth(time);
+                int day = setDay(time);
 
                 String token = FirebaseInstanceId.getInstance().getToken();
 
                 if((token = FirebaseInstanceId.getInstance().getToken())!=null) {
                     setToken(token);
                 }
-                //데이터 변화가 있을 때 시간을 측정
-                String time = getTime();
-                String month = time.substring(5,7);
-                String day = time.substring(8,10);
 
                 for (DataSnapshot messageData : dataSnapshot.getChildren()) {
                     final String title = messageData.getValue().toString();
                     //오늘 날짜의 점검이 있는지 체크
+
                     if(title.contains(month+"월"+" "+day+"일") && title.contains("점검")) {
                         //완료되었으면
-                        if(title.contains("완료")) {
+                        if(title.contains("완료") || title.contains("종료")) {
                             tv_check.setText("점검 완료");
                             layout_main.setBackgroundColor(Color.rgb(34, 164, 69));
                             //관리자 폰일 때 메세지 전송
@@ -104,9 +110,9 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                     //오늘 날짜의 점검은 없지만 어제 날짜의 점검이 있는 경우
-                    else if((title.contains(month+"월"+" "+(Integer.valueOf(day)-1)+"일"))&& title.contains("점검")){
+                    else if((title.contains(setYesterMonth(month,day)+"월"+" "+setYesterDay(year,month,day)+"일"))&& title.contains("점검")){
                         //어제 점검이 오늘 완료 되었으면
-                        if(title.contains("완료"))
+                        if(title.contains("완료") || title.contains("종료"))
                         {
                             tv_check.setText("점검 완료");
                             layout_main.setBackgroundColor(Color.rgb(34, 164, 69));
@@ -134,11 +140,13 @@ public class MainActivity extends AppCompatActivity {
                             SharedPreferences.Editor messagedata = mdata.edit();
                             messagedata.putInt("data",message);
                             messagedata.commit();
+                            break;
                         }
                     }
                     //크롤러로 공지를 최신화하지 않았거나 오늘도 어제도 점검이 없는 경우
                     else{
                         tv_check.setText("프로그램이 동작 전이거나 \n 점검이 없습니다.");
+                        layout_main.setBackgroundColor(Color.rgb(255,255,255));
                     }
                 }
             }
@@ -158,6 +166,74 @@ public class MainActivity extends AppCompatActivity {
         mNow = System.currentTimeMillis();
         mDate = new Date(mNow);
         return mFormat.format(mDate);
+    }
+
+    private int setMonth(String time){
+        int month = Integer.valueOf(time.substring(5,7));
+        return month;
+    }
+
+    private int setDay(String time){
+        int day = Integer.valueOf(time.substring(8,10));
+        return day;
+    }
+
+    private int setYesterMonth(int month,int day)
+    {
+        int ymonth=0;
+        //만약 1일이면
+        if(day==1)
+        {
+            //1달 빼줌
+            ymonth=month-1;
+            //1월이었으면 12월로 설정
+            if(ymonth==0)
+                ymonth=12;
+            return ymonth;
+        }
+        //아니라면 그대로 리턴
+        else
+        {
+            return month;
+        }
+
+    }
+    private int setYesterDay(int year, int month,int day)
+    {
+        int yday= 0;
+        //만약 1일이라면
+        if(day==1)
+        {
+            //전 달이 31일까지 있는 경우
+            if(month == 1 || month == 2 || month == 4 || month == 6 || month == 8 || month == 9 || month == 11)
+            {
+                yday=31;
+            }
+            else if(month == 3)
+            {
+                //윤년인 경우
+                if(year%4==0&&year%100!=0||year%400==0)
+                {
+                    yday=29;
+                }
+                //아닌경우
+                else
+                {
+                    yday=28;
+                }
+            }
+            //전 달이 30일까지 있는 경우
+            else
+            {
+                yday=30;
+            }
+            return yday;
+        }
+        //아니라면 그대로 리턴
+        else
+        {
+            return day;
+        }
     }
 
     private void setToken(String token){
@@ -238,39 +314,39 @@ public class MainActivity extends AppCompatActivity {
         mReference_msg.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                 for (DataSnapshot messageData : dataSnapshot.getChildren()) {
-                //final String user_token = "eDjOtS6mUJQ:APA91bGDlQa2YCvdRXP3yFCB0lR_d0oZuh_EKt32Fee28mnZF2kZSFSWCeYMuTAZkrlL_X2EerX5CP48bRFkZeLUCQw7v53_hdF0s922JEHHABKssi0soGPwDa-g-4nKpCriaktcG6-3"; //테스트
-                final String user_token = messageData.getValue().toString();
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            // FMC 메시지 생성 start
-                            JSONObject root = new JSONObject();
-                            JSONObject notification = new JSONObject();
-                            notification.put("body", server_name + " " + check_name + " " + "점검이 완료되었습니다.");
-                            notification.put("title", getString(R.string.app_name));
-                            root.put("notification", notification);
-                            root.put("to", user_token);
-                            // FMC 메시지 생성 end
+                for (DataSnapshot messageData : dataSnapshot.getChildren()) {
+                    //final String user_token = "eDjOtS6mUJQ:APA91bGDlQa2YCvdRXP3yFCB0lR_d0oZuh_EKt32Fee28mnZF2kZSFSWCeYMuTAZkrlL_X2EerX5CP48bRFkZeLUCQw7v53_hdF0s922JEHHABKssi0soGPwDa-g-4nKpCriaktcG6-3"; //테스트
+                    final String user_token = messageData.getValue().toString();
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                // FMC 메시지 생성 start
+                                JSONObject root = new JSONObject();
+                                JSONObject notification = new JSONObject();
+                                notification.put("body", server_name + " " + check_name + " " + "점검이 완료되었습니다.");
+                                notification.put("title", getString(R.string.app_name));
+                                root.put("notification", notification);
+                                root.put("to", user_token);
+                                // FMC 메시지 생성 end
 
-                            URL Url = new URL(FCM_MESSAGE_URL);
-                            HttpURLConnection conn = (HttpURLConnection) Url.openConnection();
-                            conn.setRequestMethod("POST");
-                            conn.setDoOutput(true);
-                            conn.setDoInput(true);
-                            conn.addRequestProperty("Authorization", "key=" + key);
-                            conn.setRequestProperty("Accept", "application/json");
-                            conn.setRequestProperty("Content-type", "application/json");
-                            OutputStream os = conn.getOutputStream();
-                            os.write(root.toString().getBytes("utf-8"));
-                            os.flush();
-                            conn.getResponseCode();
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                                URL Url = new URL(FCM_MESSAGE_URL);
+                                HttpURLConnection conn = (HttpURLConnection) Url.openConnection();
+                                conn.setRequestMethod("POST");
+                                conn.setDoOutput(true);
+                                conn.setDoInput(true);
+                                conn.addRequestProperty("Authorization", "key=" + key);
+                                conn.setRequestProperty("Accept", "application/json");
+                                conn.setRequestProperty("Content-type", "application/json");
+                                OutputStream os = conn.getOutputStream();
+                                os.write(root.toString().getBytes("utf-8"));
+                                os.flush();
+                                conn.getResponseCode();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
-                    }
-                }).start();
+                    }).start();
                 }
             }
 
